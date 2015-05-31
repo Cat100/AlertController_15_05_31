a865cb72b465ca4278bf3300fc763abf3b3026f7
Chris Wardman <chrisatdarley@aol.com>
2015-05-31 15:12:25 +0100

    Initial Commit

---
+++ AlertController_15_05_31/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertController_15_05_31
+//
+//  Created by Chris Wardman on 31/05/2015.
+//  Copyright (c) 2015 Chris Wardman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
